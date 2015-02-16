c75368b39c28e2f0efe715771a479feac5b01da2
Aaron Ecay <aaronecay@gmail.com>
2015-02-16 14:02:58 +0000

    initial commit

---
+++ pyccle_search/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""A program for searching for strings of text in the PYCCLE corpus"""
+
+from pyccle_search import metadata
+
+
+__version__ = metadata.version
+__author__ = metadata.authors[0]
+__license__ = metadata.license
+__copyright__ = metadata.copyright
